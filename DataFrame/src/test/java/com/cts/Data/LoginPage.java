15a899840bcfa7fe1238f0e2709df2b9896c1456
Kiran <chigurupatikiran97@gmail.com>
2020-04-11 20:15:02 +0530

    yes

---
+++ DataFrame/src/test/java/com/cts/Data/LoginPage.java
@@ -0,0 +1,18 @@
+package com.cts.Data;
+import org.testng.annotations.Test;
+
+import com.cts.DataDriven.DataLogin;
+
+
+
+
+public class LoginPage {
+	@Test
+	  public void DataLogin() 
+	  {
+		  DataLogin dd = new DataLogin();
+		  dd. startEngine();
+		  
+	  }
+	}
+
